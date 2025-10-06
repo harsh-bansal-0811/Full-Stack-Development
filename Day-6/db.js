56319b2fc539209e7a04899527bbc80b4b63d515
harsh-bansal-0811 <harsh.bansal1121@gmail.com>
2025-10-06 15:01:52 +0530

    files added

--- Day-6/db.js
+++ Day-6/db.js
@@ -3,7 +3,7 @@ async function connectDB()
 {
     try{
 await mongoose.connect(
-    "mongodb+srv://harshbansal1121_db_user:Harsh007@harshdb.7ttxdyy.mongodb.net/?retryWrites=true&w=majority&appName=HarshDB"
+    "mongodb+srv://harshbansal1121_db_user:Harsh007@harshdb.7ttxdyy.mongodb.net/SectionB?retryWrites=true&w=majority&appName=HarshDB"
 )
 console.log("mongodb connect")
     }
--- Day-6/server.js
+++ Day-6/server.js
@@ -23,6 +23,19 @@ catch(err){
 
 }
 })
+app.get("/user",async(req,res)=>{
+    try{
+const user=await User.find()
+res.json({
+    data:user
+})
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+
+})
 app.listen(4000,()=>{
 console.log("server running at port 4000")
 })
